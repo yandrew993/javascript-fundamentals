14462595d05476de8a5c3d4856879210688099e2
Andrew Young <yandrew993@gmail.com>
2023-07-22 13:03:40 +0300

    commited changes on loops

--- javascript.js
+++ javascript.js
@@ -61,7 +61,7 @@ const calcTax = function(salary){
 const pay = calcTax(10200);
 console.log(calcTax(10200));
 
-
+//Coding challenge
 //Implementation of Arrow functions
 const calcScore = (a,b,c)=>(a+b+c)/3;
 const koalasScore = calcScore(2,3,34);
@@ -97,6 +97,7 @@ const ages = [calcAge(friendsBirthYears[0]),calcAge(friendsBirthYears[1]),calcAg
 console.log(ages);
 console.log(age1,age2,age3);
 
+//Coding challenge
 const calcTip = function(bill){
    return bill>=50&&bill<=300?0.15*bill:0.2*bill;
 }
@@ -109,7 +110,7 @@ const bills = [calcTip(billValues[0]),calcTip(billValues[1]),calcTip(billValues[
 const totals = [billValues[0]+bills[0],billValues[1]+bills[1],billValues[2]+bills[2],billValues[billValues.length-1]+bills[bills.length-1]];
 console.log(billValues,bills,totals);
 //console.log(calcTip(bill1,bill2,bill3,bill4));
-*/
+
 
 //OBJECTS
 const details = {
@@ -136,5 +137,85 @@ if(details[intrestedIn]){
 }else{
     console.log(`Wrong choice please Select firstName,lastName,birthYear,Age or Course`);
 }
+
+//coding challenge
+const mark = {
+    fullName: "Mark Miller",
+    weight: 75,
+    height: 1.69,
+    calcBmi: function(){
+        return this.weight/(this.height*this.height);
+    }
+
+}
+const john = {
+    fullName: "John Smith",
+    weight: 98,
+    height: 1.95,
+    calcBmi: function(){
+        return this.weight/(this.height*this.height);
+    }
+}
+console.log(mark.calcBmi());
+console.log(john.calcBmi());
+console.log(`${john.fullName} is ${john.height}m tall and has a weight of ${john.calcBmi()}Kg`);
+//for(let i = 0;i<john.length;i++){
+//console.log(john);
+//}
+
+// Implementation of for loop with push method in arrays
+const friendsBirthYears = [2001,2003,1998,1999];
+const ages = [];
+for(let i=0;i<friendsBirthYears.length;i++){
+ ages.push(2037 - friendsBirthYears[i]);
+}
+console.log(ages);
+
+//Implementation of Continue in arrays
+
+const payments = [200,300,100,400];
+const balances = [];
+for(let i=0;i<payments.length;i++){
+    balances.push(500 - payments[i]);
+    if(balances[i]>400)continue;
+}
+console.log(balances);
+
+//implementation of BREAK
+
+const payments1 = [200,300,100,400];
+const balances1 = [];
+for(let i=0;i<payments1.length;i++){
+    balances1.push(500 - payments1[i]);
+    if(balances1[i]===400)break;
+}
+
+//Looping the arrays backwards
+
+const bankAccount = [
+    'KCB',394242,'Andrew young','Bondo',[200,300,450]
+];
+for(let i= bankAccount.length-1;i>=0;i--){
+    console.log(bankAccount[i]);
+}
+
+//Looping inside a loop
+
+for(let exercise=1;exercise<4;exercise++){
+    console.log(`----Starting exercise ${exercise}`);
+    for(let rep=1;rep<6;rep++){
+        console.log(`Lifting the gym in exercise${exercise} for the ${rep}time`);
+    }
+}
+*/
+let dice = Math.trunc(Math.random()*6)+1;
+while(dice<6){
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random()*6)+1;
+    if(dice===6){
+        console.log(`Loop is about to end`);
+    }
+}
+
 
 
