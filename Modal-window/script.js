594cd947a454e2f2145dc26759dff43b37bc7f7f
Andrew Young <yandrew993@gmail.com>
2023-07-24 19:19:46 +0300

    window-modal js code added

--- Modal-window/script.js
+++ Modal-window/script.js
@@ -1,1 +1,20 @@
 'use strict';
+//Declaration of variables
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+//Functions
+const openBtn = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeBtn = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openBtn);
+//console.log(btnsOpenModal);
+closeModal.addEventListener('click', closeBtn);
+overlay.addEventListener('click', closeBtn);
