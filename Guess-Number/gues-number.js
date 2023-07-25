6f5955d4164e7a7ffd32617954acdad7e1be70eb
Andrew Young <yandrew993@gmail.com>
2023-07-25 21:46:15 +0300

    added changes

--- Guess-Number/gues-number.js
+++ Guess-Number/gues-number.js
@@ -1,1 +1,45 @@
 "use strict";
+//document.querySelector(".number").textContent = 32;
+//document.querySelector(".guess").value = 23;
+//console.log(document.querySelector(".guess").value);
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".check").addEventListener("click", function () {
+  const gues = Number(document.querySelector(".guess").value);
+  if (!gues) {
+    document.querySelector(".message").textContent = "No number";
+    console.log(gues, typeof gues);
+  } else if (gues === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = " #60b347";
+    document.querySelector(".number").style.width = "30rem";
+  } else if (gues > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (gues < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+document.querySelector(".again").addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector("body").style.backgroundColor = " #222";
+  document.querySelector(".score").textContent = 20;
+  document.querySelector(".number").style.width = "15rem";
+});
