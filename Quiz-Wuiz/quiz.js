671f7cfc915476cfe42364ba497b8f9869871772
Andrew Young <yandrew993@gmail.com>
2023-08-04 22:22:32 +0300

    quiz-wuiz folder added

--- Quiz-Wuiz/quiz.js
+++ Quiz-Wuiz/quiz.js
@@ -1,106 +1,115 @@
+"use strict";
 //Question bank
-var questionBank= [
-    {
-        question : 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
-        option : ['Asia','Africa','Europe','Australia'],
-        answer : 'Africa'
-    },
-    {
-        question : 'Garampani sanctuary is located at',
-        option : ['Junagarh, Gujarat','Diphu, Assam','Kohima, Nagaland','Gangtok, Sikkim'],
-        answer : 'Diphu, Assam'
-    },
-    {
-        question : 'For which of the following disciplines is Nobel Prize awarded?',
-        option : ['Physics and Chemistry','Physiology or Medicine','Literature, Peace and Economics','All of the above'],
-        answer : 'All of the above'
-    },
-    {
-        question : 'Hitler party which came into power in 1933 is known as',
-        option : ['Labour Party','Nazi Party','Ku-Klux-Klan','Democratic Party'],
-        answer : 'Nazi Party'
-    },
-    {
-        question : 'First human heart transplant operation conducted by Dr. Christiaan Barnard on Louis Washkansky, was conducted in',
-        option : ['1967','1968','1958','1922'],
-        answer : '1967'
-    }
-]
+const questionBank = [
+  {
+    question:
+      "Eritrea, which became the 182nd member of the UN in 1993, is in the continent of",
+    option: ["Asia", "Africa", "Europe", "Australia"],
+    answer: "Africa",
+  },
+  {
+    question: "Garampani sanctuary is located at",
+    option: [
+      "Junagarh, Gujarat",
+      "Diphu, Assam",
+      "Kohima, Nagaland",
+      "Gangtok, Sikkim",
+    ],
+    answer: "Diphu, Assam",
+  },
+  {
+    question: "For which of the following disciplines is Nobel Prize awarded?",
+    option: [
+      "Physics and Chemistry",
+      "Physiology or Medicine",
+      "Literature, Peace and Economics",
+      "All of the above",
+    ],
+    answer: "All of the above",
+  },
+  {
+    question: "Hitler party which came into power in 1933 is known as",
+    option: ["Labour Party", "Nazi Party", "Ku-Klux-Klan", "Democratic Party"],
+    answer: "Nazi Party",
+  },
+  {
+    question:
+      "First human heart transplant operation conducted by Dr. Christiaan Barnard on Louis Washkansky, was conducted in",
+    option: ["1967", "1968", "1958", "1922"],
+    answer: "1967",
+  },
+];
 
-var question= document.getElementById('question');
-var quizContainer= document.getElementById('quiz-container');
-var scorecard= document.getElementById('scorecard');
-var option0= document.getElementById('option0');
-var option1= document.getElementById('option1');
-var option2= document.getElementById('option2');
-var option3= document.getElementById('option3');
-var next= document.querySelector('.next');
-var points= document.getElementById('score');
-var span= document.querySelectorAll('span');
-var i=0;
-var score= 0;
+let question = document.getElementById("question");
+let quizContainer = document.getElementById("quiz-container");
+let scoreboard = document.getElementById("scoreboard");
+let option0 = document.getElementById("option0");
+let option1 = document.getElementById("option1");
+let option2 = document.getElementById("option2");
+let option3 = document.getElementById("option3");
+let next = document.querySelector(".next");
+let points = document.getElementById("score");
+let span = document.querySelectorAll("span");
+let stats = document.getElementById("stat");
+let i = 0;
+let score = 0;
 
 //function to display questions
-function displayQuestion(){
-    for(var a=0;a<span.length;a++){
-        span[a].style.background='none';
-    }
-    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
-    option0.innerHTML= questionBank[i].option[0];
-    option1.innerHTML= questionBank[i].option[1];
-    option2.innerHTML= questionBank[i].option[2];
-    option3.innerHTML= questionBank[i].option[3];
-    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
-}
+const displayQuestion = function () {
+  for (let a = 0; a < span.length; a++) {
+    span[a].style.background = "none";
+  }
+  question.textContent = "Q." + (i + 1) + " " + questionBank[i].question;
+  option0.textContent = questionBank[i].option[0];
+  option1.textContent = questionBank[i].option[1];
+  option2.textContent = questionBank[i].option[2];
+  option3.textContent = questionBank[i].option[3];
+  stats.textContent =
+    "Question" + " " + (i + 1) + " " + "of" + " " + questionBank.length;
+};
 
 //function to calculate scores
-function calcScore(e){
-    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
-    {
-        score= score+1;
-        document.getElementById(e.id).style.background= 'limegreen';
-    }
-    else{
-        document.getElementById(e.id).style.background= 'tomato';
-    }
-    setTimeout(nextQuestion,300);
-}
+const calcScore = function (e) {
+  if (e.textContent === questionBank[i].answer && score < questionBank.length) {
+    score = score + 1;
+    document.getElementById(e.id).style.background = "limegreen";
+  } else {
+    document.getElementById(e.id).style.background = "tomato";
+  }
+  setTimeout(nextQuestion, 300);
+};
 
 //function to display next question
-function nextQuestion(){
-    if(i<questionBank.length-1)
-    {
-        i=i+1;
-        displayQuestion();
-    }
-    else{
-        points.innerHTML= score+ '/'+ questionBank.length;
-        quizContainer.style.display= 'none';
-        scoreboard.style.display= 'block'
-    }
+function nextQuestion() {
+  if (i < questionBank.length - 1) {
+    i = i + 1;
+    displayQuestion();
+  } else {
+    points.textContent = score + "/" + questionBank.length;
+    quizContainer.style.display = "none";
+    scoreboard.style.display = "block";
+  }
 }
 
 //click events to next button
-next.addEventListener('click',nextQuestion);
+next.addEventListener("click", nextQuestion);
 
 //Back to Quiz button event
-function backToQuiz(){
-    location.reload();
+function backToQuiz() {
+  location.reload();
 }
 
 //function to check Answers
-function checkAnswer(){
-    var answerBank= document.getElementById('answerBank');
-    var answers= document.getElementById('answers');
-    answerBank.style.display= 'block';
-    scoreboard.style.display= 'none';
-    for(var a=0;a<questionBank.length;a++)
-    {
-        var list= document.createElement('li');
-        list.innerHTML= questionBank[a].answer;
-        answers.appendChild(list);
-    }
-}
-
+const checkAnswer = function () {
+  let answerBank = document.getElementById("answerBank");
+  let answers = document.getElementById("answers");
+  answerBank.style.display = "block";
+  scoreboard.style.display = "none";
+  for (let a = 0; a < questionBank.length; a++) {
+    let list = document.createElement("li");
+    list.textContent = questionBank[a].answer;
+    answers.appendChild(list);
+  }
+};
 
 displayQuestion();
